7666a1d48c5043ebbf8778eb187097b00ade8239
mielcorpuz <jomiel.banog2x@gmail.com>
2025-07-22 22:32:03 +0800

    change icon in metadata for page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   title: 'Badger State Solutions',
   description: 'Badger State Solutions is a business management and consulting firm dedicated to helping organizations streamline operations, improve performance, and achieve sustainable growth.',
   icons: {
-    icon: '/icons/logo1.png',
+    icon: '/favicon.ico',
   },
 };
 
